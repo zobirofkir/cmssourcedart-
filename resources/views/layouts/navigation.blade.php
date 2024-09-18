da239ae321f730a5051ff394a5d362a7f94fc9da
zobirofkir <zobirofkir19@gmail.com>
2024-09-18 17:51:33 +0100

    Add Some Styles In Navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,16 +24,8 @@
                         {{ __('Reddifusion') }}
                     </x-nav-link>
 
-                    <x-nav-link :href="route('files.index')" :active="request()->routeIs('files.index')" id="exportButton" class="hover:text-indigo-500 dark:hover:text-indigo-400">
-                        {{ __('Redifusion Files') }}
-                    </x-nav-link>
-
                     <x-nav-link :href="route('items.index')" :active="request()->routeIs('items.index')" class="hover:text-indigo-500 dark:hover:text-indigo-400">
                         {{ __('Items') }}
-                    </x-nav-link>
-
-                    <x-nav-link :href="route('itemsfiles.index')" :active="request()->routeIs('itemsfiles.index')" class="hover:text-indigo-500 dark:hover:text-indigo-400">
-                        {{ __('Items Files') }}
                     </x-nav-link>
 
                     <x-nav-link :href="route('programme.index')" :active="request()->routeIs('programme.index')" class="hover:text-indigo-500 dark:hover:text-indigo-400">
@@ -44,44 +36,67 @@
                         {{ __('E-Poster') }}
                     </x-nav-link>
 
-                    <x-nav-link :href="route('e-poster.index')" :active="request()->routeIs('e-poster.index')" class="hover:text-indigo-500 dark:hover:text-indigo-400">
-                        {{ __('E-Poster Files') }}
-                    </x-nav-link>
-
                     <x-nav-link :href="route('project.export')" :active="request()->routeIs('project.export')" id="exportButton" class="hover:text-indigo-500 dark:hover:text-indigo-400">
                         {{ __('Export') }}
                     </x-nav-link>
                 </div>
             </div>
 
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-700 dark:text-gray-300 bg-gray-100 dark:bg-gray-800 hover:text-gray-800 dark:hover:text-gray-200 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-                            <div class="ms-2">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
+            <!-- Right Side -->
+            <div class="flex items-center ml-auto">
+            <!-- Dropdown Trigger -->
+            <div x-data="{ open: false }" class="relative inline-block">
+                <button @click="open = !open" class="inline-flex items-center justify-center text-gray-800 dark:text-gray-300 hover:text-indigo-500 dark:hover:text-indigo-400 focus:outline-none">
+                    <svg class="ml-2 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                    </svg>
+                </button>
+                
+                <!-- Dropdown Menu -->
+                <div x-show="open" @click.away="open = false" class="absolute right-0 bg-white dark:bg-gray-800 shadow-lg rounded-md mt-2 w-48 z-10">
+                        <x-nav-link :href="route('files.index')" :active="request()->routeIs('files.index')" class="block flex justify-center py-2 text-gray-800 dark:text-gray-300 hover:bg-indigo-500 dark:hover:bg-indigo-400 hover:text-white w-full">
+                            {{ __('Redifusion Files') }}
+                        </x-nav-link>
 
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')" class="hover:bg-gray-100 dark:hover:bg-gray-700">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault(); this.closest('form').submit();" class="hover:bg-gray-100 dark:hover:bg-gray-700">
-                                {{ __('Log Out') }}
+                        <x-nav-link :href="route('e-poster.index')" :active="request()->routeIs('e-poster.index')" class="block w-full flex justify-center py-2 text-gray-800 dark:text-gray-300 hover:bg-indigo-500 dark:hover:bg-indigo-400 hover:text-white">
+                            {{ __('E-Poster Files') }}
+                        </x-nav-link>
+
+                        <x-nav-link :href="route('itemsfiles.index')" :active="request()->routeIs('itemsfiles.index')" class="block w-full flex justify-center py-2 text-gray-800 dark:text-gray-300 hover:bg-indigo-500 dark:hover:bg-indigo-400 hover:text-white">
+                            {{ __('Items Files') }}
+                        </x-nav-link>
+                    </div>
+                </div>
+
+                <!-- Settings Dropdown -->
+                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-700 dark:text-gray-300 bg-gray-100 dark:bg-gray-800 hover:text-gray-800 dark:hover:text-gray-200 focus:outline-none transition ease-in-out duration-150">
+                                <div>{{ Auth::user()->name }}</div>
+                                <div class="ms-2">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('profile.edit')" class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                                {{ __('Profile') }}
                             </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <x-dropdown-link :href="route('logout')"
+                                        onclick="event.preventDefault(); this.closest('form').submit();" class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
             </div>
 
             <!-- Hamburger -->
@@ -95,6 +110,7 @@
             </div>
         </div>
     </div>
+
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
@@ -111,16 +127,8 @@
                 {{ __('Reddifusion') }}
             </x-responsive-nav-link>
 
-            <x-responsive-nav-link :href="route('files.index')" :active="request()->routeIs('files.index')" class="hover:bg-gray-100 dark:hover:bg-gray-800">
-                {{ __('Reddifusion Files') }}
-            </x-responsive-nav-link>
-
             <x-responsive-nav-link :href="route('items.index')" :active="request()->routeIs('items.index')" class="hover:bg-gray-100 dark:hover:bg-gray-800">
                 {{ __('Items') }}
-            </x-responsive-nav-link>
-
-            <x-responsive-nav-link :href="route('itemsfiles.index')" :active="request()->routeIs('itemsfiles.index')" class="hover:bg-gray-100 dark:hover:bg-gray-800">
-                {{ __('Items Files') }}
             </x-responsive-nav-link>
 
             <x-responsive-nav-link :href="route('programme.index')" :active="request()->routeIs('programme.index')" class="hover:bg-gray-100 dark:hover:bg-gray-800">
@@ -129,10 +137,6 @@
 
             <x-responsive-nav-link :href="route('eposter.index')" :active="request()->routeIs('eposter.index')" class="hover:bg-gray-100 dark:hover:bg-gray-800">
                 {{ __('E-Poster') }}
-            </x-responsive-nav-link>
-
-            <x-responsive-nav-link :href="route('e-poster.index')" :active="request()->routeIs('e-poster.index')" class="hover:bg-gray-100 dark:hover:bg-gray-800">
-                {{ __('E-Poster Files') }}
             </x-responsive-nav-link>
 
 
